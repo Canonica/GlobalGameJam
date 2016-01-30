ccdfe27d217ec957faa0991b6104424132c99013
Cédric Brun <brun.cedric2233@gmail.com>
2016-01-30 09:59:35 +0100

    Je sais plus

--- GlobalGameJam/Assets/Scripts/Player/Player.cs
+++ GlobalGameJam/Assets/Scripts/Player/Player.cs
@@ -22,7 +22,18 @@ public class Player : MonoBehaviour
 
     void Hit()
     {
-        Vector3 hitDirection = Input.mousePosition - transform.position;
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y,Camera.main.transform.position.z);
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10000))
+        {
+            
+        }
+        Debug.Log(hit.point);
+
+        Vector3 hitDirection = new Vector3(hit.point.x, hit.point.y, 0) - transform.position;
+        
         Enemy enemy;
         Vector3 v;
         float produitScalaire;
@@ -31,9 +42,13 @@ public class Player : MonoBehaviour
         {
             enemy = instanceEM.mEnemies[i];
             v = enemy.transform.position - transform.position;
+            //Debug.Log("u : " + hitDirection);
+            //Debug.Log("v : " + v);
             produitScalaire = hitDirection.x * v.x + hitDirection.y * v.y + hitDirection.z * v.z;
+            //Debug.Log("prodScal : " + produitScalaire);
             angle = Mathf.Acos(produitScalaire / (hitDirection.magnitude * v.magnitude));
-            
+            //Debug.Log("angle : " + angle);
+
             if (Mathf.Sqrt(angle* angle) < Mathf.Sqrt(angleHit * angleHit))
             {
                 instanceEM.mEnemies.Remove(enemy);
