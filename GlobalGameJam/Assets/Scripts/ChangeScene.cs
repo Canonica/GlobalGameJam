2c739450b951a35c9202ef88361cbee818c7901b
KeumarSIG <m.bonte@rubika-edu.com>
2016-01-30 10:21:30 +0100

    Merging Marc
    - Changer scene
    - Activer bool dans l'arène
    - Rajouter éléments dans la maison

---
+++ GlobalGameJam/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+	void Awake ()
+	{
+		DontDestroyOnLoad(this); // makes sure this gameobject moves between the scenes
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			LoadMenu();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			LoadHome();
+		}
+
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			LoadArena();
+		}
+	}
+
+	// Functions used to change scenes
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("MainMenu"); // Go to Menu
+	}
+
+	public void LoadHome()
+	{
+		SceneManager.LoadScene("Home"); // Go to Home
+	}
+
+	public void LoadArena()
+	{
+		SceneManager.LoadScene("Arena"); // Go to Arena
+	}
+}
